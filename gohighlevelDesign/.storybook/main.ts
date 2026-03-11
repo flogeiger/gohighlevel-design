15e2959258bbf823c6b5b07b49b8f864216a2167
Florain Geiger <florian.geiger21@gmail.com>
2026-03-11 21:53:37 +0100

    Fix: addon-docs/blocks import + leerzeilen + main.ts cleanup

--- gohighlevelDesign/.storybook/main.ts
+++ gohighlevelDesign/.storybook/main.ts
@@ -20,25 +20,6 @@ const config: StorybookConfig = {
   ],
   framework: "@storybook-vue/nuxt",
   staticDirs: ["../public"],
-
-  viteFinal: async (config) => {
-    config.build = config.build ?? {};
-    config.build.rollupOptions = config.build.rollupOptions ?? {};
-
-    const existing = config.build.rollupOptions.external;
-    const asArray = Array.isArray(existing)
-      ? existing
-      : existing
-      ? [existing]
-      : [];
-
-    config.build.rollupOptions.external = [
-      ...asArray,
-      '@storybook/blocks',
-    ];
-
-    return config;
-  },
 };
 
 export default config;
